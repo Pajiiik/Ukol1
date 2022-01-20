55cc9ed3d789ea247871927b8b5350be0935413e
Pavel <86227916+Pajiiik@users.noreply.github.com>
2022-01-20 16:52:31 +0100

    Add files via upload

---
+++ P-klad1/17.01/17.01/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace _17._01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.Cislo1 = new System.Windows.Forms.TextBox();
+            this.Zadani = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label1.Location = new System.Drawing.Point(3, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Zadej cislo";
+            // 
+            // Cislo1
+            // 
+            this.Cislo1.Location = new System.Drawing.Point(67, 3);
+            this.Cislo1.Name = "Cislo1";
+            this.Cislo1.Size = new System.Drawing.Size(330, 20);
+            this.Cislo1.TabIndex = 1;
+            this.Cislo1.TextChanged += new System.EventHandler(this.Cislo1_TextChanged);
+            this.Cislo1.MouseEnter += new System.EventHandler(this.Cislo1_MouseEnter);
+            this.Cislo1.MouseLeave += new System.EventHandler(this.Cislo1_MouseLeave);
+            // 
+            // Zadani
+            // 
+            this.Zadani.BackColor = System.Drawing.SystemColors.Control;
+            this.Zadani.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.Zadani.Location = new System.Drawing.Point(3, 3);
+            this.Zadani.Name = "Zadani";
+            this.Zadani.Size = new System.Drawing.Size(394, 52);
+            this.Zadani.TabIndex = 2;
+            this.Zadani.Text = "Vypocet";
+            this.Zadani.UseVisualStyleBackColor = false;
+            this.Zadani.Click += new System.EventHandler(this.Zadani_Click);
+            this.Zadani.MouseEnter += new System.EventHandler(this.Zadani_MouseHover);
+            this.Zadani.MouseLeave += new System.EventHandler(this.Zadani_MouseLeave);
+            this.Zadani.MouseHover += new System.EventHandler(this.Zadani_MouseHover);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(6, 11);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(394, 173);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.Click += new System.EventHandler(this.listBox1_Click);
+            this.listBox1.MouseEnter += new System.EventHandler(this.listBox1_MouseEnter);
+            this.listBox1.MouseLeave += new System.EventHandler(this.listBox1_MouseLeave);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.RosyBrown;
+            this.flowLayoutPanel1.Controls.Add(this.Zadani);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 46);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(405, 59);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.RosyBrown;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.Cislo1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(405, 28);
+            this.panel1.TabIndex = 4;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.RosyBrown;
+            this.panel2.Controls.Add(this.listBox1);
+            this.panel2.Location = new System.Drawing.Point(12, 111);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(405, 195);
+            this.panel2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.ClientSize = new System.Drawing.Size(426, 318);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Nabidka";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Cislo1;
+        private System.Windows.Forms.Button Zadani;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ P-klad1/17.01/17.01/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _17._01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int cislo;
+        int Cislo_Dva;
+
+        string nevim;
+        private void Zadani_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            try
+            {        
+                int vysledek = 0;
+                nevim = Cislo1.Text;
+                cislo = Int32.Parse(nevim);
+                    Cislo_Dva = cislo;
+                    int i = 0;
+                    if (cislo < 0)
+                    {
+                        MessageBox.Show("Špatně zadáno!");
+                        Cislo1.Clear();
+                    }
+                    else
+                    {
+                        while (cislo > 0)
+                        {
+                            int pocitadlo = 0;
+                            while (pocitadlo == 0)
+                            {
+
+                                vysledek = (cislo % 10) + vysledek;
+                                pocitadlo++;
+                            }
+                            cislo = cislo / 10;
+                        }
+                        MessageBox.Show(vysledek.ToString(), "ciferný součet");
+                        Cislo1.Clear();
+                        while (i != Cislo_Dva)
+                        {
+                            i++;
+                            if (Cislo_Dva % i == 0)
+                            {
+                                listBox1.Items.Add(i.ToString());
+                            }
+                        }
+                    }
+            }
+            catch
+            {
+                MessageBox.Show("Špatně zadáno!");
+                Cislo1.Clear();
+            }
+
+
+        }
+
+        private void Cislo1_TextChanged(object sender, EventArgs e)
+        {
+           Cislo1.BackColor = Color.White;
+        }
+
+        private void Zadani_MouseHover(object sender, EventArgs e)
+        {
+            Zadani.BackColor = Color.LightGray;
+        }
+
+        private void Zadani_MouseLeave(object sender, EventArgs e)
+        {
+            Zadani.BackColor = Color.White;
+        }
+
+        private void Cislo1_MouseEnter(object sender, EventArgs e)
+        {
+        }
+
+        private void Cislo1_MouseLeave(object sender, EventArgs e)
+        {
+            Cislo1.BackColor = Color.White;
+        }
+
+        private void listBox1_MouseEnter(object sender, EventArgs e)
+        {
+            listBox1.Font = new Font("Microsoft Sans Serif;", 12, FontStyle.Bold);
+        }
+
+        private void listBox1_MouseLeave(object sender, EventArgs e)
+        {
+            listBox1.Font = new Font("Microsoft Sans Serif;", 8);
+        }
+
+        private void listBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
